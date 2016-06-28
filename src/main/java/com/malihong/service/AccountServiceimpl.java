f5edaf27b03f15539a64890d3c790d3c3992940e
gshhunter <gshhunter@gmail.com>
2016-06-28 10:43:56 +1000

    Transactional 事务提交问题

--- src/main/java/com/malihong/service/AccountServiceimpl.java
+++ src/main/java/com/malihong/service/AccountServiceimpl.java
@@ -2,6 +2,7 @@ package com.malihong.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.malihong.dao.AccountDao;
 import com.malihong.dao.IdentDao;
@@ -11,6 +12,7 @@ import com.malihong.entity.Identification;
 import com.malihong.entity.Profile;
 
 @Service("accountService")
+@Transactional
 public class AccountServiceimpl implements AccountService{
 
 	@Autowired
