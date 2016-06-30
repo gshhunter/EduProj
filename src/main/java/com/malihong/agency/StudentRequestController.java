e82eaef076472f7d679d6da534589a0065711ed8
Lingkai.Xu <xlk910728@gmail.com>
2016-06-30 17:19:41 +1000

    post&get by json

--- src/main/java/com/malihong/agency/StudentRequestController.java
+++ src/main/java/com/malihong/agency/StudentRequestController.java
@@ -6,10 +6,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.malihong.entity.Request;
+import com.malihong.entity.University;
 import com.malihong.service.StudentRequestService;
+
+import java.io.IOException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,27 +34,36 @@ public class StudentRequestController {
 	@Autowired
 	private StudentRequestService reqService;
 	
-	@RequestMapping(value = "/post", method = RequestMethod.POST,produces = "application/json")
-	public String newRequest(@RequestBody Request r){
-		logger.info(r.toString());
-		//this.reqService.save(r);
-		return "fortest";
+	@RequestMapping(value = "/post", method = RequestMethod.POST)
+	public @ResponseBody University newRequest(@RequestBody String r) throws JsonParseException, JsonMappingException, IOException{
+		ObjectMapper mapper = new ObjectMapper();
+		University u=mapper.readValue(r, University.class);
+		//University u=new University();
+		u.setCity(u.getCity()+"123");
+		String s = mapper.writeValueAsString(u);
+
+		//logger.info(obj.name);
+
+		return u;
 	}
 	
 	@RequestMapping(value = "/get", method = RequestMethod.GET)
-	public @ResponseBody Shop newtest(){
+	public @ResponseBody Shop newtest(@RequestParam String aaa) throws JsonProcessingException{
 		logger.info("get!");
 		Shop shop=new Shop();
-		shop.name="sss";
+		shop.name=aaa;
 		shop.staffName=new String[]{"mkyong1", "mkyong2"};
-		//this.reqService.save(r);
+
 		return shop;
 	}
 	
 	public class Shop {
 
-		String name;
-		String staffName[];
+		public Shop(){
+			
+		};
+		public String name;
+		public String staffName[];
 		
 	}
 }
