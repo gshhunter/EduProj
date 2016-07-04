cbd2e4dccdb9137227c4191778ed12ca5e0c4382
gshhunter <gshhunter@gmail.com>
2016-07-04 18:08:29 +1000

    New Tomcat

--- src/main/java/com/malihong/agency/StudentRequestController.java
+++ src/main/java/com/malihong/agency/StudentRequestController.java
@@ -1,9 +1,7 @@
 package com.malihong.agency;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,8 +11,6 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.malihong.entity.Request;
 import com.malihong.entity.University;
 import com.malihong.service.StudentRequestService;
 
