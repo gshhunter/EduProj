e7cd2824996169b2037125cf966c3977a86bf4a4
gshhunter <gshhunter@gmail.com>
2016-07-04 17:35:17 +1000

    resolve

--- src/main/java/com/malihong/agency/HomeController.java
+++ src/main/java/com/malihong/agency/HomeController.java
@@ -6,18 +6,10 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.malihong.entity.Account;
-
-import com.malihong.entity.Profile;
-
-import com.malihong.service.AccountService;
-import com.malihong.service.UniversityService;
 
 /**
  * Handles requests for the application home page.
@@ -26,20 +18,14 @@ import com.malihong.service.UniversityService;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	@Autowired
-	private UniversityService universityService;
-	@Autowired
-	private AccountService accountService;
+
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		String city=universityService.findUniversityById(2).getCity();
-		logger.info(city);
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
@@ -60,10 +46,10 @@ public class HomeController {
 		//Testing Account addUser function - end
 			
 		//Testing Account addUser function
-			Account account1 = accountService.findUserById(3);
-			Profile p = account1.getProfile();
-			p.setCountry("Dayang");
-			accountService.update(account1);
+//			Account account1 = accountService.findUserById(3);
+//			Profile p = account1.getProfile();
+//			p.setCountry("Dayang");
+//			accountService.update(account1);
 		//Testing Account addUser function - end
 		
 		String formattedDate = dateFormat.format(date);
---
+++ src/main/java/com/malihong/agency/StudentRequestController.java
@@ -0,0 +1,69 @@
+package com.malihong.agency;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.malihong.entity.Request;
+import com.malihong.entity.University;
+import com.malihong.service.StudentRequestService;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@Controller
+@RequestMapping(value = "/req")
+public class StudentRequestController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(StudentRequestController.class);
+
+	
+	@Autowired
+	private StudentRequestService reqService;
+	
+	@RequestMapping(value = "/post", method = RequestMethod.POST)
+	public @ResponseBody University newRequest(@RequestBody String r) throws JsonParseException, JsonMappingException, IOException{
+		ObjectMapper mapper = new ObjectMapper();
+		University u=mapper.readValue(r, University.class);
+		//University u=new University();
+		u.setCity(u.getCity()+"123");
+		String s = mapper.writeValueAsString(u);
+
+		//logger.info(obj.name);
+
+		return u;
+	}
+	
+	@RequestMapping(value = "/get", method = RequestMethod.GET)
+	public @ResponseBody Shop newtest(@RequestParam String aaa) throws JsonProcessingException{
+		logger.info("get!");
+		Shop shop=new Shop();
+		shop.name=aaa;
+		shop.staffName=new String[]{"mkyong1", "mkyong2"};
+
+		return shop;
+	}
+	
+	public class Shop {
+
+		public Shop(){
+			
+		};
+		public String name;
+		public String staffName[];
+		
+	}
+}
