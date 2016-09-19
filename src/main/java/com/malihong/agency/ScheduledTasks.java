6deaef5c8353ca3bf48c9d9446c330ec9b7475bb
Lingkai.Xu <xlk910728@gmail.com>
2016-09-19 15:33:19 +1000

    ADD： task schedule

---
+++ src/main/java/com/malihong/agency/ScheduledTasks.java
@@ -0,0 +1,12 @@
+package com.malihong.agency;
+
+import org.springframework.scheduling.annotation.Scheduled;    
+import org.springframework.stereotype.Component;  
+
+@Component("tasks")
+public class ScheduledTasks {
+    @Scheduled(cron = "0/5 * * * * ?")  
+    public void job1() {  
+        System.out.println("scheduled task 1");
+    }
+}
