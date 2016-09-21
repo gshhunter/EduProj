84fbe011ad3e72c0cd50d286016023e93c2e1856
Lingkai.Xu <xlk910728@gmail.com>
2016-09-21 15:33:22 +1000

    REMOVE: tasks

--- src/main/java/com/malihong/agency/ScheduledTasks.java
+++ src/main/java/com/malihong/agency/ScheduledTasks.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Component;
 
 @Component("tasks")
 public class ScheduledTasks {
-    @Scheduled(cron = "0/5 * * * * ?")  
+/*    @Scheduled(cron = "0/5 * * * * ?")  
     public void job1() {  
         System.out.println("scheduled task 1");
-    }
+    }*/
 }
